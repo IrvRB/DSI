97631eb26d1d80a34c70d0b4ebe82a167d2a05b7
ElmarcianitoGtR <carlosrafaelfabianjimenez1@gmai.com>
2026-04-29 13:29:05 -0500

    Update: files tree

--- DSI30/delete/dDomicilio.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../assets/controlador.php';
-$id_domicilio = $_POST['id_domicilio'];
-$resultejecutar = ejecutar("DELETE FROM domicilios WHERE idDomicilio = ?", [
-    $id_domicilio
-]);
--- DSI30/delete/dagente.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../assets/controlador.php';
-$id_agente = $_POST['id_agente'];
-$resultejecutar = ejecutar("DELETE FROM agentes WHERE idAgente = ?", [
-    $id_agente
-]);
