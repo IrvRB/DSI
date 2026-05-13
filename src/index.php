2d3a7954b4568c2dd1415dc9379157105a7b225e
Jaguar <jaguar@Parasemamol.skullbrain>
2026-05-13 14:16:53 -0600

    Fix:Principal redirection

--- src/index.php
+++ src/index.php
@@ -1,11 +1,12 @@
-/*<?php
-        if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-                $uri = 'https://';
-        } else {
-                $uri = 'http://';
-        }
-        $uri .= $_SERVER['HTTP_HOST'];
-        header('Location: '.$uri.'/DSI30/');
-        exit;
+<?php
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+} else {
+    $uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+
+// Si solo quieres ir a la carpeta actual, no te compliques:
+header('Location: '.$uri.'/DSI30/'); 
+exit;
 ?>
-*/
